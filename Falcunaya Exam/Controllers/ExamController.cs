bc4ac7e709a6c4d523de837154a923a9acb120a4
billyjayfalcunaya-hue <billyjayfalcunaya@gmail.com>
2026-09-16 17:26:42 +0800

    Answer no.5

--- Falcunaya Exam/Controllers/ExamController.cs
+++ Falcunaya Exam/Controllers/ExamController.cs
@@ -31,7 +31,12 @@ namespace ExamApp.Controllers
                 Options = new() { "A. DbContext", "B. DbSetView", "C. ControllerContext", "D. RazorContext" },
                 CorrectAnswer = "A. DbContext"
             },
-
+            new ExamQuestion {
+                Number = 5,
+                Question = "What does the following command primarily do? dotnet ef dbcontext scaffold \"ConnectionString\" Microsoft.EntityFrameworkCore.SqlServer -o Models",
+                Options = new() { "A. Deletes the database", "B. Creates a new MVC project", "C. Generates EF Core models and a DbContext from an existing database", "D. Starts the MVC application" },
+                CorrectAnswer = "C. Generates EF Core models and a DbContext from an existing database"
+            },
         };
 
         public IActionResult Index(string? search)
